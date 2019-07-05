0967f3854a7a1e627832b1ff772b28fc70795fd5
deceitya <49361292+deceitya@users.noreply.github.com>
2019-07-05 18:49:23 +0900

    データ保存

---
+++ src/algiadesign/moneyalgia/provider/Provider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace algiadesign\moneyalgia\provider;
+
+use algiadesign\moneyalgia\MoneyalgiaPlugin;
+
+/**
+ * データ保存
+ *
+ * @author deceitya
+ */
+interface Provider
+{
+    /**
+     * コンストラクタ
+     *
+     * @param MoneyalgiaPlugin プラグイン
+     */
+    public function __construct(MoneyalgiaPlugin $plugin);
+
+    /**
+     * データベースを開く
+     *
+     * @return boolean 開けたらtrue、それ以外はfalse
+     */
+    public function open(): bool;
+
+    /**
+     * アカウントが存在しているか
+     *
+     * @param string $id ユーザー名
+     * @return boolean 存在していればtrue、それ以外はfalse
+     */
+    public function accountExists(string $id): bool;
+
+    /**
+     * アカウント作成
+     *
+     * @param string  $id     ユーザー名
+     * @param integer $amount 初期値
+     * @return boolean 作成成功でtrue、それ以外はfalse
+     */
+    public function createAccount(string $id, int $amount): bool;
+
+    /**
+     * アカウント削除
+     *
+     * @param string $id ユーザー名
+     * @return boolean 削除成功でtrue、それ以外はfalse
+     */
+    public function removeAccount(string $id): bool;
+
+    /**
+     * ユーザーが所持しているお金の量を取得
+     *
+     * @param string $id ユーザー名
+     * @return integer|null お金の量、アカウントが存在しない場合null
+     */
+    public function getAmount(string $id): ?int;
+
+    /**
+     * ユーザーが所持するお金の量を変更
+     *
+     * @param string  $id     ユーザー名
+     * @param integer $amount お金の量
+     * @return boolean 変更成功でtrue、それ以外はfalse
+     */
+    public function setAmount(string $id, int $amount): bool;
+
+    /**
+     * 全ユーザーの所持金の量を配列で返す
+     *
+     * @return array
+     */
+    public function getAll(): array;
+
+    /**
+     * セーブ
+     *
+     * @return boolean セーブ成功でtrue、それ以外はfalse
+     */
+    public function save(): bool;
+
+    /**
+     * リソース開放
+     *
+     * @return boolean 解放成功でtrue、それ以外はfalse
+     */
+    public function close(): bool;
+}
---
+++ src/algiadesign/moneyalgia/provider/SQLiteProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace algiadesign\moneyalgia\provider;
+
+use algiadesign\moneyalgia\MoneyalgiaPlugin;
+use SQLite3;
+
+class SQLiteProvider implements Provider
+{
+    /** @var MoneyalgiaPlugin */
+    private $plugin;
+    /** @var SQLite3 */
+    private $db;
+
+    public function __construct(MoneyalgiaPlugin $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function open(): bool
+    {
+        $file = $this->plugin->getDataFolder() . "moneyalgia.db";
+        if (file_exists($file)) {
+            $this->db = new SQLite3($file, SQLITE3_OPEN_READWRITE);
+        } else {
+            $this->db = new SQLite3($file, SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+        }
+
+        return $this->db->exec('CREATE TABLE IF NOT EXISTS money (id TEXT PRIMARY KEY NOT NULL, amount INTEGER NOT NULL)');
+    }
+
+    public function accountExists(string $id): bool
+    {
+        $stmt = $this->db->prepare("SELECT * FROM money WHERE id = :id");
+        $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+
+        return \count($stmt->execute()->fetchArray(SQLITE3_ASSOC)) > 0 ? true: false;
+    }
+
+    public function createAccount(string $id, int $amount): bool
+    {
+        if (!$this->accountExists($id)) {
+            $stmt = $this->db->prepare("INSERT INTO money (id, amount) VALUES (:id, :amount)");
+            $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+            $stmt->bindValue(':amount', $amount, SQLITE3_INTEGER);
+
+            return $stmt->execute() === false ? false : true;
+        }
+
+        return false;
+    }
+
+    public function removeAccount(string $id): bool
+    {
+        $stmt = $this->db->prepare("DELETE FROM money id = :id");
+        $stmt->bindValue(':id', $id);
+
+        return $stmt->execute() === false ? false : true;
+    }
+
+    public function getAmount(string $id): ?int
+    {
+        $stmt = $this->db->prepare("SELECT amount FROM money WHERE id = :id");
+        $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+        $result = $stmt->execute()->fetchArray(SQLITE3_ASSOC);
+
+        return empty($result) ? null : $result["amount"];
+    }
+
+    public function setAmount(string $id, int $amount): bool
+    {
+        $stmt = $this->db->prepare("UPDATE money SET amount = :amount WHERE id = :id");
+        $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+        $stmt->bindvalue(':amount', $amount, SQLITE3_INTEGER);
+
+        return $stmt->execute() === false ? false : true;
+    }
+
+    public function getAll(): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM money");
+
+        $result = [];
+        foreach ($stmt->execute()->fetchArray(SQLITE3_ASSOC) as $val) {
+            $result[$val[0]] = $val[1];
+        }
+
+        return $result;
+    }
+
+    public function save(): bool
+    {
+        return true;
+    }
+
+    public function close(): bool
+    {
+        if ($this->db instanceof SQLite3) {
+            $this->db->close();
+
+            return true;
+        }
+
+        return false;
+    }
+}
